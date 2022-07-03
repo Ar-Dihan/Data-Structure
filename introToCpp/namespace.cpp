3294692b5940bbff60e01970af2c0e8727449b5c
Ar-Dihan <arnahid18@gmail.com>
2022-07-03 23:40:35 +0600

    namespace file added

---
+++ introToCpp/namespace.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+namespace name
+{
+    void print()
+    {
+        cout << "working";
+    }
+}
+using namespace name;
+int main()
+{
+    print();
+
+    return 0;
+}
