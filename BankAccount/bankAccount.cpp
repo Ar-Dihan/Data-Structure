761d1600c6786bff7180a7763947b272a76a596c
Ar-Dihan <arnahid18@gmail.com>
2022-07-22 20:59:02 +0600

    bank constructed

--- BankAccount/bankAccount.cpp
+++ BankAccount/bankAccount.cpp
@@ -1,6 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+class BankAccount
+{
+public:
+    string account_holder;
+    string address;
+    int age;
+    int account_number;
+
+protected:
+    int balance;
+
+private:
+    string password;
+
+public:
+    BankAccount(string account_holder, string address, int age, string password)
+    {
+        this->account_holder = account_holder;
+        this->address = address;
+        this->age = age;
+        this->password = password;
+        this->account_number = rand() % 1000000000;
+        cout << "Yout ac num. is" << this->account_number;
+    }
+};
+
 int main()
 {
 
