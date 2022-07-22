7818c8c4400da7a677cac02538a4df926cac68be
Ar-Dihan <arnahid18@gmail.com>
2022-07-22 21:31:39 +0600

    account creation done

--- BankAccount/bankAccount.cpp
+++ BankAccount/bankAccount.cpp
@@ -23,12 +23,44 @@ public:
         this->age = age;
         this->password = password;
         this->account_number = rand() % 1000000000;
-        cout << "Yout ac num. is" << this->account_number;
+        this->balance = 100;
+        cout << "Yout ac num. is " << this->account_number << endl;
+    }
+
+    int show_balance(string password)
+    {
+        if (this->password == password)
+        {
+            return balance;
+        }
+        else
+        {
+            return -1;
+        }
     }
 };
 
+BankAccount *createAccount()
+{
+    string account_holder, address, password;
+    int age;
+    cout << "Create Account" << endl;
+    cin >> account_holder >> address >> age >> password;
+    BankAccount *myAccount = new BankAccount(account_holder, address, age, password);
+    return myAccount;
+}
+
 int main()
 {
+    BankAccount *myAccount = createAccount();
+    if (myAccount->show_balance("abc") == -1)
+    {
+        cout << "password didnt matched" << endl;
+    }
 
+    else
+    {
+        cout << "Your bank balance is " << myAccount->show_balance("abc") << endl;
+    }
     return 0;
 }
