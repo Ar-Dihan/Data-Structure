231c346ffbf906612d86305e2bac8a7b5a43492e
Ar-Dihan <arnahid18@gmail.com>
2022-07-29 01:16:48 +0600

    A prob solved

--- module12Exam/A_-_A.cpp
+++ module12Exam/A_-_A.cpp
@@ -1,8 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int challenger()
+{
+    int arr[4], count = 0;
+    for (int i = 0; i < 4; i++)
+    {
+        cin >> arr[i];
+        if (arr[i] >= 10)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
 int main()
 {
-
+    cout << challenger();
     return 0;
 }
